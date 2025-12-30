32c47489e6a3c2b667231dd828bfce8d9da38e2e
christianbpryor <christianbpryor@gmail.com>
2025-12-30 21:40:30 +0000

    Deploying to gh-pages from @ christianbpryor/christianbpryor.github.io@271ad38bbc207569b0a4126a2ace98e316e9172f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,12 +23,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
+        },{id: "nav-gallery",
+          title: "gallery",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/projects/";
+            window.location.href = "/gallery/";
           },
         },{id: "nav-repositories",
           title: "repositories",
