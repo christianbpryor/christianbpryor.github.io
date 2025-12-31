005e20f38cdb7f7e73b3ffb0d2177b9daaa14505
christianbpryor <christianbpryor@gmail.com>
2025-12-31 07:50:07 +0000

    Deploying to gh-pages from @ christianbpryor/christianbpryor.github.io@1beb1bbcc67f0d6e7f8c97cf6d004b07913ee848 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -481,9 +481,9 @@ ninja.data = [{
           description: "a project with no image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
+            },},{id: "projects-boncat",
+          title: 'BONCAT',
+          description: "Undergraduate Research 2022-2023",
           section: "Projects",handler: () => {
               window.location.href = "/projects/7_project/";
             },},{id: "projects-project-8",
